8bb980ddb18f64498557a2af7909cc0c6e350a28
0poison <106107068+0poison@users.noreply.github.com>
2022-08-31 17:50:48 +0900

    Add files via upload

--- web_study_07/src/main/java/model/MemberDAO.java
+++ web_study_07/src/main/java/model/MemberDAO.java
@@ -6,29 +6,32 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.Vector;
 
-public class MemberDAO { /* Database */
+public class MemberDAO {
 	String id = "scott";
 	String pass = "tiger";
-	String url = "jdbc:oracle:thin:@localhost:1521:xe";
+	String url = "jdbc:oracle:thin:@localhost:1521:XE";
 
 	Connection conn;
 	PreparedStatement pstmt;
 	ResultSet rs;
 
-	public void getCon() {
+	public void getcon() {
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			conn = DriverManager.getConnection(url, id, pass);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+
 	}
 
-	public void insertMember(MemberBean mbean) {
+	public void insertMember(MemberBean mbean) { // DB에 데이터를 넣는 메서드
+
 		try {
-			getCon();
-			String sql = "insert into memberInfo values(?,?,?,?,?,?,?,?)";
+			getcon();
+			String sql = "INSERT INTO memberINFO VALUES(?,?,?,?,?,?,?,?)";
 			pstmt = conn.prepareStatement(sql);
+
 			pstmt.setString(1, mbean.getId());
 			pstmt.setString(2, mbean.getPass1());
 			pstmt.setString(3, mbean.getEmail());
@@ -37,25 +40,22 @@ public class MemberDAO { /* Database */
 			pstmt.setString(6, mbean.getJob());
 			pstmt.setString(7, mbean.getAge());
 			pstmt.setString(8, mbean.getInfo());
-			pstmt.executeUpdate();
-
-			conn.close();
-			pstmt.close();
-
+			pstmt.executeUpdate(); // insert, update, delete
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+
 	}
 
 	public Vector<MemberBean> allSelectMember() {
-//가변 길이로 데이터를 저장 프로그램이 종료할 때까지 사라지지 않도록 static을 붙인다.
-		Vector<MemberBean> vec = new Vector<MemberBean>();
+		Vector<MemberBean> vec = new Vector<>();
 
 		try {
-			getCon();
-			String sql = "select * from memberInfo";
+			getcon();
+			String sql = "SELECT * FROM memberInfo";
 			pstmt = conn.prepareStatement(sql);
 			rs = pstmt.executeQuery();
+
 			while (rs.next()) {
 				MemberBean mbean = new MemberBean();
 				mbean.setId(rs.getString(1));
@@ -66,35 +66,40 @@ public class MemberDAO { /* Database */
 				mbean.setJob(rs.getString(6));
 				mbean.setAge(rs.getString(7));
 				mbean.setInfo(rs.getString(8));
+
 				vec.add(mbean);
 			}
 			conn.close();
 			pstmt.close();
 			rs.close();
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+
 		return vec;
 	}
 
 	public MemberBean oneSelectMember(String id) {
+
 		MemberBean mbean = new MemberBean();
+
 		try {
-			getCon();
-			String sql = "select * from memberInfo where id=mbean.setId";
+			getcon();
+			String sql = "SELECT * FROM memberInfo WHERE id=?";
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, id);
 			rs = pstmt.executeQuery();
-			if (rs.next()) {
-				mbean.setId(rs.getString(1));
-				mbean.setId(rs.getString(2));
-				mbean.setId(rs.getString(3));
-				mbean.setId(rs.getString(4));
-				mbean.setId(rs.getString(5));
-				mbean.setId(rs.getString(6));
-				mbean.setId(rs.getString(7));
-				mbean.setId(rs.getString(8));
-			}
+			rs.next();
+			mbean.setId(rs.getString(1));
+			mbean.setPass1(rs.getString(2));
+			mbean.setEmail(rs.getString(3));
+			mbean.setTel(rs.getString(4));
+			mbean.setHobby(rs.getString(5));
+			mbean.setJob(rs.getString(6));
+			mbean.setAge(rs.getString(7));
+			mbean.setInfo(rs.getString(8));
+
 			conn.close();
 			pstmt.close();
 			rs.close();
@@ -105,28 +110,36 @@ public class MemberDAO { /* Database */
 	}
 
 	public void updateMember(MemberBean bean) {
-		getCon();
+		getcon();
 		try {
-			String sql = "update memberInfo set email=?, tel=? where id=?";
+			String sql = "UPDATE memberInfo SET email=?, tel=? WHERE id=?";
 			pstmt = conn.prepareStatement(sql);
-			rs = pstmt.executeQuery();
+
 			pstmt.setString(1, bean.getEmail());
 			pstmt.setString(2, bean.getTel());
 			pstmt.setString(3, bean.getId());
+			pstmt.executeUpdate();
 
+			conn.close();
+			pstmt.close();
+			rs.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+
 	}
 
 	public String getPass1(String id) {
+
 		String pass = "";
-		getCon();
+		getcon();
+
 		try {
-			String sql = "select pass1 from memberInfo where id=?";
+			String sql = "SELECT pass1 FROM memberInfo WHERE id=?";
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, id);
 			rs = pstmt.executeQuery();
+
 			if (rs.next()) {
 				pass = rs.getString(1);
 			}
@@ -137,6 +150,25 @@ public class MemberDAO { /* Database */
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+
 		return pass;
 	}
+
+	public void deleteMember(String id) {
+		getcon();
+
+		try {
+			String sql = "DELETE FROM memberInfo WHERE id=?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, id);
+			pstmt.executeUpdate();
+
+			conn.close();
+			pstmt.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 }
